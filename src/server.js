8a202aa4c0f4a9f092ddd696ecf9ebe8aa9c5af3
NEIL LOPES JOÃO FILHO <neil.filho@portalsesisp.org.br>
2026-07-31 15:55:29 -0300

    atualizando v3

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
 require('dotenv-safe').config();
 const app = require('./app');
 const logger = require('./utils/logger'); 
-const iniciarCronJobs = require('./cron'); // Mantemos apenas o nosso HUB centralizado
 
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   logger.info(`🚀 Servidor rodando na porta ${PORT}`);
   logger.info(`🌍 Ambiente: ${process.env.NODE_ENV}`);
-  
-  // Inicia a nossa central de robôs em background (Cozinha, Faltas, Backups)
-  iniciarCronJobs(); 
-  
-  logger.info('⏳ Central de Cron Jobs ativada com sucesso.');
 });
 
 // Tratamento de interrupções para desligar o servidor de forma limpa (Graceful Shutdown)
